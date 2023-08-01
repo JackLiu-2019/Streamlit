8a60e72804406479334865134509cd4ea0504a17
JackLiu-2019 <54927909+JackLiu-2019@users.noreply.github.com>
2023-08-01 22:14:17 +0800

    Update streamlit_demo.py
    update upload file function

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,6 +1,5 @@
 """
 # My first App
-Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
@@ -8,7 +7,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import time
-
+import os
 
 #Basic function demo
 st.title('This is a title')
@@ -16,6 +15,33 @@ st.header('This is a header')
 st.subheader('This is a subheader')
 st.text('This is a text')
 st.markdown('Streamlit is **_really_ cool**.')
+
+path = r'./data'
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+uploadedfile = st.file_uploader('File uploader')
+if uploadedfile is not None:
+    with open(r'./data/'+uploadedfile.name, 'wb') as f:
+        f.write(uploadedfile.getbuffer())
+
+data_entry = st.text_area('Area for textual entry')
+with open('./data/data.txt', 'a+') as file:
+    file.write(data_entry + '\n')
+
+# 如果int, 指定要插入的列数，并且所有列的宽度相等。
+# 如果一个数字列表, 为每个数字创建一列，每列的宽度与提供的数字成比例。数字可以是整数或浮点数，但它们必须是正数。
+# 下例中col1占表格1/6宽度，col2占表格1/6宽度，col3占表格4/6宽度，
+col1, col2, col3 = st.columns([1,1,4])
+with col1:
+    #如果st.button被点击
+    if st.button('Clear'):
+        with open('./data/data.txt', 'w') as file:
+            file.write('')
+with col2:
+    with open('./data/data.txt') as f:
+        st.download_button('Download', f)
 
 code = '''
     def hello():
@@ -29,7 +55,7 @@ data = 'just a test'
 if st.button('Hit me'):
     st.write('Button is clicked')
 
-st.download_button('On the dl', data)
+# st.download_button('On the dl', data)
 st.checkbox('Check me out')
 
 # st.radio('Radio', [1,2,3])
@@ -44,10 +70,10 @@ st.slider('Slide me', min_value=0, max_value=10)
 st.select_slider('Slide to select', options=[1,'2'])
 st.text_input('Enter some text')
 st.number_input('Enter a number')
-st.text_area('Area for textual entry')
+# st.text_area('Area for textual entry')
 st.date_input('Date input')
 st.time_input('Time entry')
-st.file_uploader('File uploader')
+# st.file_uploader('File uploader')
 # st.camera_input("一二三,茄子!")
 st.color_picker('Pick a color')
 
